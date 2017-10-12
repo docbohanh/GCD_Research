686d4b799a7bbb8b498af7f06cff7e37754369a6
MacMini <>
2017-10-12 15:48:20 +0700

    Init GCD example project

---
+++ GCD_Demo/GCD_Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GCD_Demo
+//
+//  Created by MacMini on 10/12/17.
+//  Copyright © 2017 Thành Lã. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
